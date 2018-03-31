f0a7507669437503df62c9b4134682b4f43c21b9
codeatzip <ai.spring.1956@gmail.com>
2018-03-31 11:44:05 +0530

    Completed section 2 of udemy machine learning course

--- code/udemy/Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ code/udemy/Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -4,16 +4,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import Imputer
+from sklearn.cross_validation import train_test_split
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+data_set = pd.read_csv('Data.csv')
+X = data_set.iloc[:,:-1].values
+y = data_set.iloc[:,-1].values
 
-X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1].values
 
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+# Splitting the dataset into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-imputer = imputer.fit(X[:,1:3])
-
-X[:,1:3] = imputer.transform(X[:,1:3])
+# Feature Scaling
+"""sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
---
+++ code/udemy/Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template_complete.py
@@ -0,0 +1,35 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Importing the dataset
+data_set = pd.read_csv('Data.csv')
+X = data_set.iloc[:,:-1].values
+y = data_set.iloc[:,-1].values
+
+# Taking care of missing data
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encoding categorical data
+label_encoder_X = LabelEncoder()
+X[:,0] = label_encoder_X.fit_transform(X[:,0])
+one_hot_encoder = OneHotEncoder(categorical_features = [0])
+X = one_hot_encoder.fit_transform(X).toarray()
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
+# Splitting the dataset into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
