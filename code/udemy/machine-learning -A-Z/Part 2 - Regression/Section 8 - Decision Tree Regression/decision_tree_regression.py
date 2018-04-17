e591c76f35e63e08166e55af8ab0cc84b1889f7a
codeatzip <ai.spring.1956@gmail.com>
2018-04-17 10:38:49 +0530

    Completed Decision Tree Regression of udemy course(Section 8)

--- code/udemy/machine-learning -A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
+++ code/udemy/machine-learning -A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -23,19 +24,18 @@ sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Decision Tree Regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X, y)
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X,y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_predict = regressor.predict(6.5)
 
 # Visualising the Decision Tree Regression results (higher resolution)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position level')
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff(Decision Tree Regression)')
+plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
